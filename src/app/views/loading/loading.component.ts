2e3b6c1e783c110f1cd61c492275e19a509c4758
Isis Jaime <97555627+IsisJaimeV@users.noreply.github.com>
2022-07-13 16:12:13 -0500

    Change AES security

--- src/app/views/loading/loading.component.ts
+++ src/app/views/loading/loading.component.ts
@@ -28,23 +28,31 @@ export class LoadingComponent implements OnInit {
     }, 2000);
   }
 
-  authUser() {
+  authUser(){
     var path = document.URL;
-
+    
     var correoTemp = path.split('?correo=')[1];
+    var tokenTemp = path.split('&token=')[1];
 
     var correo = correoTemp.split('&token=')[0];
-    var token = path.split('&token=')[1];
+    var token = tokenTemp.split('&origen=')[0];
 
+    var indexCidrado = path.indexOf("&origen=");
+    var cifrado = undefined;
+    if ( indexCidrado >= 0){
+      cifrado = path.split('&origen=')[1];
+    }else{
+      cifrado = undefined;
+    }
 
     var pathJSON = {
       correo: decodeURIComponent(correo),
       token: decodeURIComponent(token),
-      sourceId: 1
+      origen: cifrado,
+      sourceId: 3
     }
 
     this.loading = true;
-    localStorage.setItem("user", "")
     this.precioPiso.getAuth(pathJSON).subscribe(res => {
       if (res.resultado == true) {
         localStorage.setItem("user", res.correo)
@@ -58,6 +66,5 @@ export class LoadingComponent implements OnInit {
       this.router.navigate(['not-found']);
     })
 
-    return this.user;
   }
 }
